9c9476a62edf8aab64e54f4c27900c238d5769ec
ruslan-tykhonov <RUSLANEGOROVICH1943@GMAIL.COM>
2025-02-18 16:51:04 +0100

    change

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-import s from "./ErrorMessage.module.css";
-
-export default function ErrorMessage({ children }) {
-  return (
-    <div>
-      <p className={s.error}>{children}</p>
-    </div>
-  );
-}
--- src/components/ImageCard/ImageCard.jsx
+++
@@ -1,9 +0,0 @@
-import s from "./ImageCard.module.css";
-
-export default function ImageCard({ data }) {
-  return (
-    <div className={s.item}>
-      <img src={data.urls.small} alt={data.description} className={s.img} />
-    </div>
-  );
-}
